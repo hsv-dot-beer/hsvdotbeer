bdcf472627eec213976bc21088274a1e000360bb
Drew Winstel <drewbrew@users.noreply.github.com>
2018-11-04 14:08:30 -0600

    Create trivial viewset for Venue
    Does not deal with auth or permissions yet.

--- venues/views.py
+++ venues/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from . import serializers
+from . import models
+
+
+class VenueViewSet(ModelViewSet):
+    serializer_class = serializers.VenueSerializer
+    queryset = models.Venue.objects.order_by('name')
