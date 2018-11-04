0e125170b27dfaaad823f072b9b156855fbc8726
Drew Winstel <drewbrew@users.noreply.github.com>
2018-11-04 14:28:37 -0600

    wire in venues to urlconf

--- hsv_dot_beer/urls.py
+++ hsv_dot_beer/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
 from rest_framework.authtoken import views
+
 from .users.views import UserViewSet, UserCreateViewSet
 
 router = DefaultRouter()
@@ -14,6 +15,7 @@ router.register(r'users', UserCreateViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include(router.urls)),
+    path('api/v1/venues/', include('venues.urls')),
     path('api-token-auth/', views.obtain_auth_token),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
