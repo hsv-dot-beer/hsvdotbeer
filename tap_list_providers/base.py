185fea7a02770ef09ba42d73e7349b5377b5ac8b
Drew Winstel <drewbrew@users.noreply.github.com>
2019-02-01 21:43:14 -0600

    Convert ABV from a string to a decimal if needed

--- tap_list_providers/base.py
+++ tap_list_providers/base.py
@@ -5,12 +5,16 @@ which takes a single argument: a Venue object.
 
 It'll have API configuration, rooms, taps, and existing beers prefetched.
 """
+from decimal import Decimal
+import logging
 
 from django.db.models import Prefetch
 
 from venues.models import Venue, Room
 from beers.models import Beer
 from taps.models import Tap
+
+LOG = logging.getLogger(__name__)
 
 
 class BaseTapListProvider():
@@ -27,6 +31,8 @@ class BaseTapListProvider():
         if not self.provider_name:
             raise ValueError(
                 'You must define `provider_name` in your __init__()')
+        LOG.debug(
+            'Looking for venues with tap list provider %s', self.provider_name)
         queryset = Venue.objects.select_related(
             'api_configuration',
         ).filter(
@@ -57,6 +63,16 @@ class BaseTapListProvider():
         bogus_defaults = set(defaults).difference(field_names)
         if bogus_defaults:
             raise ValueError(f'Unknown fields f{",".join(sorted(defaults))}')
+        try:
+            abv = defaults.pop('abv')
+        except KeyError:
+            pass
+        else:
+            if isinstance(abv, str):
+                if abv.endswith('%'):
+                    abv = abv[:-1]
+                abv = Decimal(abv)
+            defaults['abv'] = abv
         beer = Beer.objects.get_or_create(
             name=name,
             manufacturer=manufacturer,
