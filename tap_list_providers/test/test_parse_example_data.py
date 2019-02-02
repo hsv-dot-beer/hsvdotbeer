88d0f896c0c47bbeca17b2115c81315345425bfe
Drew Winstel <drewbrew@users.noreply.github.com>
2019-02-01 22:14:35 -0600

    Run the JSON test twice to make sure we don't double-create

--- tap_list_providers/test/test_parse_example_data.py
+++ tap_list_providers/test/test_parse_example_data.py
@@ -1,4 +1,5 @@
 """Test the parsing of example data"""
+from decimal import Decimal
 
 from django.core.management import call_command
 from django.test import TestCase
@@ -26,20 +27,24 @@ class CommandsTestCase(TestCase):
 
     def test_import_data(self):
         """Test parsing the JSON data"""
-
         self.assertFalse(Tap.objects.exists())
         self.assertEqual(Venue.objects.count(), 1)
         self.assertFalse(Beer.objects.exists())
         self.assertFalse(Manufacturer.objects.exists())
-        args = []
-        opts = {}
-        call_command('parseexampletaplist', *args, **opts)
+        for dummy in range(2):
+            # running twice to make sure we're not double-creating
+            args = []
+            opts = {}
+            call_command('parseexampletaplist', *args, **opts)
 
-        self.assertEqual(Beer.objects.count(), 3)
-        self.assertEqual(Manufacturer.objects.count(), 3)
-        self.assertEqual(Tap.objects.count(), 3)
-        tap = Tap.objects.filter(room=self.room, tap_number=1).select_related(
-            'beer__style',
-        ).get()
-        self.assertEqual(tap.beer.name, "Monkeynaut")
-        self.assertEqual(tap.beer.style.name, 'American IPA')
+            self.assertEqual(Beer.objects.count(), 3)
+            self.assertEqual(Manufacturer.objects.count(), 3)
+            self.assertEqual(Tap.objects.count(), 3)
+            tap = Tap.objects.filter(
+                room=self.room, tap_number=1,
+            ).select_related(
+                'beer__style',
+            ).get()
+            self.assertEqual(tap.beer.name, "Monkeynaut")
+            self.assertEqual(tap.beer.abv, Decimal('7.25'))
+            self.assertEqual(tap.beer.style.name, 'American IPA')
