66c50b27fac7c091aee86af7a9ed717fcf40a82e
Drew Winstel <drewbrew@users.noreply.github.com>
2019-02-05 17:01:47 -0600

    Add model fields for digital pour and untappd venues

--- venues/models.py
+++ venues/models.py
@@ -2,6 +2,7 @@
 
 from django.db import models
 from django.conf import settings
+from django.contrib.postgres.fields import ArrayField
 from timezone_field.fields import TimeZoneField
 from django_countries.fields import CountryField
 
@@ -46,6 +47,16 @@ class VenueAPIConfiguration(models.Model):
     )
     url = models.URLField()
     api_key = models.CharField(max_length=100, blank=True)
+    digital_pour_venue_id = models.CharField(max_length=50, blank=True)
+    digital_pour_location_number = models.PositiveSmallIntegerField(
+        blank=True, null=True,
+    )
+    untappd_location = models.PositiveIntegerField(blank=True, null=True)
+    untappd_theme = models.PositiveIntegerField(blank=True, null=True)
+    untappd_categories = ArrayField(
+        models.CharField(max_length=50),
+        default=list,
+    )
 
 
 class Room(models.Model):
