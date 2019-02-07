ec28a051e04dce10f49772e66ff1f4630a4f6403
Drew Winstel <drewbrew@users.noreply.github.com>
2019-02-07 08:10:44 -0600

    Add parser and tests for the Nook (#40)
    Fixes #19.

---
+++ tap_list_providers/management/commands/parsethenook.py
@@ -0,0 +1,26 @@
+"""Parse the nook tap list"""
+
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from beers.models import BeerStyle
+from tap_list_providers.parsers.thenook import NookParser
+
+
+class Command(BaseCommand):
+    help = 'Populates any venues using the Nook tap list provider with' \
+        ' beers'
+
+    def add_arguments(self, parser):
+        # does not take any arguments
+        pass
+
+    def handle(self, *args, **options):
+        if not BeerStyle.objects.exists():
+            raise ValueError('You must import BJCP styles before continuing')
+        tap_list_provider = NookParser()
+        with transaction.atomic():
+            for venue in tap_list_provider.get_venues():
+                self.stdout.write('Processing %s' % venue.name)
+                tap_list_provider.handle_venue(venue)
+        self.stdout.write(self.style.SUCCESS('Done!'))
