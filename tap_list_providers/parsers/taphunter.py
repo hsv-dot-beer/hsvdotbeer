f7cb9595ad146ad8122f96a99f49708115907691
Michael Carroll <carroll.michael@gmail.com>
2019-02-12 20:06:13 -0600

    parser improvements.

--- tap_list_providers/parsers/taphunter.py
+++ tap_list_providers/parsers/taphunter.py
@@ -11,37 +11,77 @@ class TaphunterParser:
         data = requests.get(self.location_url).text
         self.json = json.loads(data)
 
-    def taps(self):
-        ret= []
-        for tap in self.json['taps']:
-            tap_info = {
-                "added": tap['date_added_iso8601'],
-                "updated": tap['location']['update_timestamp_iso8601'],
-                "beer": {
-                    "name": tap['beer']['beer_name'],
-                    "manufacturer": {
-                        "name": tap['brewery']['name'],
-                        "location": tap['brewery']['origin']
-                    },
-                    "style": {
-                        "name": tap['beer']['style'],
-                        "category": tap['beer']['style_category'],
-                    }
-                }
+    def parse_beer(self, tap):
+        beer = {
+            'name': tap['beer']['beer_name'],
+            'style': {
+                'name': tap['beer']['style'],
+                'category': tap['beer']['style_category'],
             }
+        }
 
-            if tap['serving_info']['tap_number']:
-                tap_info['tap_number'] = tap['serving_info']['tap_number']
+        if tap['beer']['abv']:
+            beer['abv'] = float(tap['beer']['abv'])
 
-            if tap['beer']['abv']:
-                tap_info['beer']['abv'] = float(tap['beer']['abv'])
+        if tap['beer']['srm']:
+            beer['srm'] = int(tap['beer']['srm'])
 
-            if tap['beer']['srm']:
-                tap_info['beer']['srm'] = int(tap['beer']['srm'])
+        if tap['beer']['ibu']:
+            beer['ibu'] = float(tap['beer']['ibu'])
 
-            if tap['beer']['ibu']:
-                tap_info['beer']['ibu'] = float(tap['beer']['ibu'])
+        return beer
 
+    def parse_manufacturer(self, tap):
+        manufacturer = {
+            'name': tap['brewery']['name'],
+            'location': tap['brewery']['origin']
+        }
+        return manufacturer
+
+    def parse_tap(self, tap):
+        t = {
+            'added': tap['date_added_iso8601'],
+            'updated': tap['location']['update_timestamp_iso8601']
+        }
+        if tap['serving_info']['tap_number']:
+            t['tap_number'] = tap['serving_info']['tap_number']
+        return t
+
+    def parse_size(self, size):
+        if size == '1/6 Barrel':
+            return 660
+        elif size == '1/4 Barrel':
+            return 996
+        elif size == '1/2 Barrel':
+            return 1980
+        else:
+            return int(size.split('oz')[0])
+
+    def parse_price(self, price):
+        price = price.replace('$', '')
+        return float(price)
+
+    def parse_pricing(self, tap):
+        pricing = []
+        if 'serving_info' in tap:
+            if 'sized_pricing' in tap['serving_info']:
+                p = tap['serving_info']['sized_pricing']
+                for entry in p:
+                    price = {
+                        'size': self.parse_size(entry['size']),
+                        'price': self.parse_price(entry['price'])
+                    }
+                    price['per_ounce'] = price['price'] / price['size']
+                    pricing.append(price)
+        return pricing
+
+    def taps(self):
+        ret = []
+        for tap in self.json['taps']:
+            tap_info = self.parse_tap(tap)
+            tap_info['beer'] = self.parse_beer(tap)
+            tap_info['manufacturer'] = self.parse_manufacturer(tap)
+            tap_info['pricing'] = self.parse_pricing(tap)
             ret.append(tap_info)
         return ret
 
@@ -66,7 +106,4 @@ if __name__ == '__main__':
         print(json.dumps(t.json, indent=4))
     else:
         for tap in t.taps():
-            print(tap)
-
-
-
+            print(json.dumps(tap, indent=4))
