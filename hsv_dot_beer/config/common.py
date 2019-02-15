ae4378821dc88d728d3b1613a8ab04354cf81a13
Drew Winstel <drewbrew@users.noreply.github.com>
2019-02-14 20:05:14 -0600

    Add filtering for venues
    Fixes #52

--- hsv_dot_beer/config/common.py
+++ hsv_dot_beer/config/common.py
@@ -203,7 +203,10 @@ class Common(Configuration):
         'DEFAULT_AUTHENTICATION_CLASSES': (
             'rest_framework.authentication.SessionAuthentication',
             'rest_framework.authentication.TokenAuthentication',
-        )
+        ),
+        'DEFAULT_FILTER_BACKENDS': (
+            'django_filters.rest_framework.DjangoFilterBackend',
+        ),
     }
 
     # Default Venue time zone
