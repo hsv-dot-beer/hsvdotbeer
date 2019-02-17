f4acb9fa58b63267f4f22371163961ea10f51d0a
Michael Carroll <michael@openrobotics.org>
2019-02-17 09:17:12 -0600

    Remove pipenv

--- bin/entry-point.sh
+++ bin/entry-point.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-pipenv run gunicorn hsv_dot_beer.wsgi --log-file - &
-pipenv run celery -A hsv_dot_beer worker -l info &
-pipenv run celery -A hsv_dot_beer beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+gunicorn hsv_dot_beer.wsgi --log-file - &
+celery -A hsv_dot_beer worker -l info &
+celery -A hsv_dot_beer beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
