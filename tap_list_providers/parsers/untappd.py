66d70496033d91d89ad653174bd16593f818aead
Drew Winstel <drewbrew@users.noreply.github.com>
2019-02-18 16:11:28 -0600

    Save beer logo in untappd processing

--- tap_list_providers/parsers/untappd.py
+++ tap_list_providers/parsers/untappd.py
@@ -180,6 +180,7 @@ class UntappdParser(BaseTapListProvider):
             'div', {'class': 'label-image-hideable beer-label pull-left'},
         )
         beer_link_tag = beer_link.find('a')
+        beer_image = beer_link.find('img').attrs['src']
         if beer_link_tag:
             url = beer_link_tag.attrs['href']
         else:
@@ -206,6 +207,7 @@ class UntappdParser(BaseTapListProvider):
                 'name': beer_info,
                 'untappd_url': url,
                 'style': self.parse_style(beer_style),
+                'logo_url': beer_image,
             },
             'manufacturer':
             {
