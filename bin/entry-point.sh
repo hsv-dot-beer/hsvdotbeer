c0671bc2fdf89ba2fe85429d5efe609785f41710
Drew Winstel <drewbrew@users.noreply.github.com>
2019-02-21 11:00:38 -0600

    Launch celery beat from within the worker
    This should free up some RAM on the heroku dyno

--- bin/entry-point.sh
+++ bin/entry-point.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
 gunicorn hsv_dot_beer.wsgi --log-file - &
-celery -A hsv_dot_beer worker -l info &
-celery -A hsv_dot_beer beat -l info --scheduler django_celery_beat.schedulers:DatabaseScheduler
+celery -A hsv_dot_beer worker -l info  --beat --scheduler django_celery_beat.schedulers:DatabaseScheduler
