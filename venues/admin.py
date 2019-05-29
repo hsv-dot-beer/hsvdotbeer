21e1a169253d01d39e0dd0faaba78f7021942d7e
Drew Winstel <drewbrew@users.noreply.github.com>
2019-05-28 20:28:46 -0500

    Improve venue API configuration admin view
    Add the venue name in the admin list

--- venues/admin.py
+++ venues/admin.py
@@ -7,5 +7,10 @@ class VenueAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("name",)}
 
 
+class VenueAPIConfigurationAdmin(admin.ModelAdmin):
+    list_display = ('id', 'venue')
+    list_select_related = ('venue', )
+
+
 admin.site.register(models.Venue, VenueAdmin)
-admin.site.register(models.VenueAPIConfiguration)
+admin.site.register(models.VenueAPIConfiguration, VenueAPIConfigurationAdmin)
