b20c6936a97d35d350368fde243855641f698e0e
Drew Winstel <drewbrew@users.noreply.github.com>
2019-06-09 17:12:38 -0500

    Check the rate limit timestamp before looking up a beer
    Also add tests

--- tap_list_providers/models.py
+++ tap_list_providers/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 from venues.models import Venue
 
+
 class APIRateLimitTimestamp(models.Model):
     api_type = models.CharField(
         max_length=50, unique=True, choices=Venue.TAP_LIST_PROVIDERS,
