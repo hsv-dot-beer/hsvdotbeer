d36165ec627ec0deea28f62ef02cb5059d76c5ea
Drew Winstel <drewbrew@users.noreply.github.com>
2019-06-09 17:09:16 -0500

    Create model for storing API rate limit timestamp

--- tap_list_providers/models.py
+++ tap_list_providers/models.py
@@ -1,1 +1,10 @@
 """Models for tap list provider support"""
+from django.db import models
+
+from venues.models import Venue
+
+class APIRateLimitTimestamp(models.Model):
+    api_type = models.CharField(
+        max_length=50, unique=True, choices=Venue.TAP_LIST_PROVIDERS,
+    )
+    rate_limit_expires_at = models.DateTimeField()
