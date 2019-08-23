9146d88a98d8583833872dd13a88b33bde2f40e4
Drew Winstel <drewbrew@users.noreply.github.com>
2019-08-22 19:34:29 -0500

    make the slug lookup a little more DRY (#238)

--- venues/views.py
+++ venues/views.py
@@ -6,7 +6,8 @@ from rest_framework.permissions import IsAdminUser
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
-from beers.views import CachedListMixin
+from beers.views import CachedListMixin, BeerViewSet
+from beers.filters import BeerFilterSet
 from . import serializers
 from . import models
 from . import filters
@@ -19,13 +20,27 @@ class VenueViewSet(CachedListMixin, ModelViewSet):
 
     @method_decorator(cache_page(60 * 5))
     @action(detail=True, methods=['GET'])
-    def beers(self, request, pk):
-        from beers.views import BeerViewSet
-        from beers.filters import BeerFilterSet
-
-        queryset = BeerViewSet.queryset.filter(
-            taps__venue__id=pk,
-        ).distinct()
+    def beers(self, request, pk=None, slug=None):
+        filter_cond = {}
+        if pk and slug:
+            raise serializers.serializers.ValidationError({
+                'non_field_errors': [
+                    "Somehow you managed to submit both a PK and a slug. "
+                    "This isn't possible.",
+                ]
+            })
+        if pk:
+            filter_cond['taps__venue__id'] = pk
+        elif slug:
+            filter_cond['taps__venue__slug'] = slug
+        else:
+            raise serializers.serializers.ValidationError({
+                'non_field_errors': [
+                    "Somehow you managed to submit neither a PK nor a slug. "
+                    "This isn't possible.",
+                ]
+            })
+        queryset = BeerViewSet.queryset.filter(**filter_cond).distinct()
 
         # let the user use all the beer filters just for kicks
         queryset = BeerFilterSet(request.query_params, queryset=queryset).qs
@@ -42,26 +57,6 @@ class VenueBySlugViewSet(VenueViewSet):
 
     def list(self, request, *args, **kwargs):
         raise NotFound()
-
-    @method_decorator(cache_page(60 * 5))
-    @action(detail=True, methods=['GET'])
-    def beers(self, request, slug):
-        from beers.views import BeerViewSet
-        from beers.filters import BeerFilterSet
-
-        queryset = BeerViewSet.queryset.filter(
-            **{f'taps__venue__{self.lookup_field}': slug}
-        ).distinct()
-
-        # let the user use all the beer filters just for kicks
-        queryset = BeerFilterSet(request.query_params, queryset=queryset).qs
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = BeerViewSet.serializer_class(page, many=True)
-            return self.get_paginated_response(serializer.data)
-        serializer = BeerViewSet.serializer_class(queryset, many=True)
-        return Response(serializer.data)
 
     lookup_field = 'slug'
     serializer_class = serializers.VenueBySlugSerializer
