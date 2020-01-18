411318541dfa956639e33776003a1c4ddb87deca
Drew Winstel <drewbrew@users.noreply.github.com>
2020-01-18 15:32:00 -0600

    Upgrade to bmemcached 0.3.0 (#308)
    Also add a unit test to make sure our config is never bogus again.

--- hsv_dot_beer/config/production.py
+++ hsv_dot_beer/config/production.py
@@ -23,26 +23,8 @@ def get_cache():
         'TIMEOUT': None,
         'LOCATION': servers,
         'OPTIONS': {
-          'binary': True,
           'username': username,
           'password': password,
-          'behaviors': {
-            # Enable faster IO
-            'no_block': True,
-            'tcp_nodelay': True,
-            # Keep connection alive
-            'tcp_keepalive': True,
-            # Timeout settings
-            'connect_timeout': 2000,  # ms
-            'send_timeout': 750 * 1000,  # us
-            'receive_timeout': 750 * 1000,  # us
-            '_poll_timeout': 2000,  # ms
-            # Better failover
-            'ketama': True,
-            'remove_failed': 1,
-            'retry_timeout': 2,
-            'dead_timeout': 30,
-          }
         }
       }
     }
