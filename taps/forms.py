25bff939e19b7ba507efd466105c3c1767d929d4
Drew Winstel <drewbrew@users.noreply.github.com>
2020-10-26 16:12:37 -0500

    run black

--- taps/forms.py
+++ taps/forms.py
@@ -6,7 +6,7 @@ from .models import Tap
 
 class TapForm(ModelForm):
     def __init__(self, *args, **kwargs):
-        manufacturer = kwargs.pop('manufacturer', None)
+        manufacturer = kwargs.pop("manufacturer", None)
         super().__init__(*args, **kwargs)
         if manufacturer:
             self.fields["beer"].queryset = Beer.objects.filter(
