ca3c3f304f0871471deafa661faa87341b2198e8
Drew Winstel <drewbrew@users.noreply.github.com>
2020-11-16 19:44:44 -0600

    Parse arryved embedded menus

---
+++ tap_list_providers/parsers/arryved_menu.py
@@ -0,0 +1,271 @@
+"""Parser for Arryved embedded menus"""
+from collections import defaultdict
+import datetime
+from decimal import Decimal
+import logging
+import os
+from typing import Dict, Union, Any, List
+
+import requests
+import configurations
+from django.core.exceptions import ImproperlyConfigured, AppRegistryNotReady
+from pytz import UTC
+
+# boilerplate code necessary for launching outside manage.py
+try:
+    from ..base import BaseTapListProvider
+except (ImproperlyConfigured, AppRegistryNotReady):
+    os.environ["DJANGO_SETTINGS_MODULE"] = "hsv_dot_beer.config"
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
+    configurations.setup()
+    from ..base import BaseTapListProvider
+
+from beers.models import Manufacturer, Beer, BeerPrice, ServingSize
+from taps.models import Tap
+from venues.models import Venue
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ArryvedMenuParser(BaseTapListProvider):
+    """Parser for Arrvyed embedded menus"""
+
+    provider_name = "arryved_embedded_menu"
+    URL = "https://menu.arryved.com/preauthAction/getMenu"
+
+    def __init__(
+        self,
+        manufacturer: Manufacturer = None,
+        location_id: str = None,
+        menu_id: str = None,
+        serving_sizes: List[str] = None,
+    ):
+        self.location_id = location_id
+        self.menu_id = menu_id
+        self.manufacturer = manufacturer
+        self.serving_sizes = set(serving_sizes) if serving_sizes else set()
+
+    def handle_venue(self, venue: Venue) -> datetime.datetime:
+        self.location_id = venue.api_configuration.arryved_location_id
+        self.menu_id = venue.api_configuration.arryved_menu_id
+        self.serving_sizes = set(venue.api_configuration.arryved_serving_sizes)
+        BeerPrice.objects.filter(venue=venue).delete()
+        try:
+            self.manufacturer = Manufacturer.objects.get(
+                name=venue.api_configuration.arryved_manufacturer_name
+            )
+        except Manufacturer.DoesNotExist as exc:
+            raise ValueError(
+                "You must create a manufacturer for "
+                f"{venue.api_configuration.arryved_manufacturer_name or venue} before"
+                " parsing!"
+            ) from exc
+        json_data = self.parse_json(self.fetch())
+        taps = {i.tap_number: i for i in venue.taps.all()}
+        manufacturer_beers = {
+            beer.name: beer
+            for beer in Beer.objects.filter(manufacturer=self.manufacturer)
+        }
+        last_updated = UTC.localize(datetime.datetime(1970, 1, 1, 0))
+        prices = []
+        serving_sizes = {i.volume_oz: i for i in ServingSize.objects.all()}
+        tap_number = 1
+        for beer in json_data["beers"]:
+            tap = taps.get(tap_number, Tap(tap_number=tap_number, venue=venue))
+            existing_beer_pk = tap.beer_id
+            beer_created = False
+            try:
+                tap.beer = manufacturer_beers[beer["name"]]
+            except KeyError:
+                try:
+                    tap.beer = Beer.objects.get(
+                        manufacturer=self.manufacturer,
+                        alternate_names__name=beer["name"],
+                    )
+                except Beer.DoesNotExist:
+                    LOG.info("Creating beer %s for %s", beer["name"], self.manufacturer)
+                    new_beer = Beer.objects.create(
+                        manufacturer=self.manufacturer,
+                        ibu=beer["ibu"],
+                        abv=beer["abv"],
+                        name=beer["name"],
+                    )
+                    tap.beer = new_beer
+                    beer_created = True
+            tap_prices = self.process_serving_sizes(
+                tap.beer, venue, beer["serving_sizes"], serving_sizes
+            )
+            if not tap_prices:
+                LOG.debug('Skipping beer %s because it is not on tap', tap.beer)
+                if beer_created:
+                    tap.beer.delete()
+                continue
+            prices += tap_prices
+            if tap.beer.id != existing_beer_pk:
+                tap.time_added = beer["last_updated"]
+            if beer["ibu"]:
+                tap.beer.ibu = beer["ibu"]
+            if beer["abv"]:
+                tap.beer.abv = beer["abv"]
+            tap.time_updated = beer["last_updated"]
+            LOG.debug("Saving tap %s: %s", tap.tap_number, tap.beer)
+            tap.save()
+            if beer["last_updated"] > last_updated:
+                last_updated = beer["last_updated"]
+            tap_number += 1
+        BeerPrice.objects.bulk_create(prices)
+        self.check_timestamp = last_updated
+        return last_updated
+
+    def process_serving_sizes(
+        self,
+        beer: Beer,
+        venue: Venue,
+        pricing_data: List[Dict],
+        serving_sizes: Dict[Decimal, ServingSize],
+    ) -> List[BeerPrice]:
+        result = []
+        for size_dict in pricing_data:
+            LOG.debug("size dict: %s", size_dict)
+            if size_dict["size"]["short_code"] not in self.serving_sizes:
+                LOG.debug("Skipping serving size %s", size_dict["size"]["short_code"])
+                continue
+            capacity = Decimal(size_dict["size"]["serving_size"])
+            price = size_dict["price"]
+            try:
+                serving_size = serving_sizes[Decimal(size_dict["size"]["serving_size"])]
+            except KeyError:
+                serving_size = ServingSize.objects.create(
+                    name=f"{capacity} oz",
+                    volume_oz=capacity,
+                )
+                serving_sizes[capacity] = serving_size
+            result.append(
+                BeerPrice(
+                    beer=beer,
+                    venue=venue,
+                    serving_size=serving_size,
+                    price=price,
+                )
+            )
+            LOG.debug(
+                "Beer %s (%s) costs %s for serving size %s (PK %s)",
+                beer.id,
+                beer.name,
+                price,
+                capacity,
+                serving_size.id,
+            )
+        return result
+
+    def fetch(self):
+        response = requests.post(
+            self.URL,
+            json={
+                "locationId": self.location_id,
+                "venueIds": [],
+                "itemTypes": [],
+                "publicConfigType": "WEBSITE_EMBED",
+                "menuId": self.menu_id,
+            },
+        )
+        response.raise_for_status()
+        return response.json()
+
+    def parse_json(self, json_data: Dict[str, Any]):
+        payload = json_data["payload"]
+        sizes = self.parse_sizes(payload)
+        item_types = self.parse_item_types(payload)
+        last_updated = datetime.datetime.fromtimestamp(
+            payload["lastUpdatedAt"] / 1000, UTC
+        )
+        beers = self.parse_items(payload, sizes)
+        return {
+            "beers": beers,
+            "last_updated": last_updated,
+            "item_types": item_types,
+        }
+
+    def parse_sizes(
+        self, payload: Dict[str, Dict]
+    ) -> Dict[str, Union[str, int, float]]:
+        """Parse serving sizes"""
+        result = {
+            short_code: {
+                "serving_size": data["quantityMicros"] / 1e6,
+                "name": data["display"],
+                "short_code": short_code,
+            }
+            for short_code, data in payload["sizes"].items()
+            if not data["retired"]
+        }
+        return result
+
+    def parse_item_types(self, payload: Dict[str, Dict]) -> Dict[str, str]:
+        return {
+            short_code: data["name"].title()
+            for short_code, data in payload["itemTypes"].items()
+            if not data["retired"]
+        }
+
+    def parse_item_available_sizes(
+        self, payload: Dict[str, Dict]
+    ) -> Dict[str, List[Dict[str, Union[str, int, float]]]]:
+        sizes = self.parse_sizes(payload)
+        result = defaultdict(list)
+        for line in payload["activeItemSizes"]:
+            key, short_code = line.split(":::")
+            result[key].append(sizes[short_code])
+        return result
+
+    def parse_items(
+        self,
+        payload: Dict[str, Dict],
+        sizes: Dict[str, Union[str, int, float]],
+    ) -> List[Dict[str, Union[str, int, float]]]:
+        result = []
+        for item in payload["items"]:
+            result.append(
+                {
+                    "pk": item["id"],
+                    "name": item["displayDetails"]["name"],
+                    "alternate_name": item["displayDetails"]["shortName"],
+                    "serving_sizes": [
+                        {
+                            "size": sizes[cost["size"]],
+                            "price": cost["cost"]["micros"] / 1e6,
+                        }
+                        for cost in item["itemCosts"]
+                        if cost["state"] == "ACTIVE"
+                    ],
+                    "abv": self.get_abv(item["attributes"]),
+                    "ibu": self.get_ibu(item["attributes"]),
+                    "last_updated": datetime.datetime.fromtimestamp(
+                        item["lastUpdateTime"] / 1000, UTC
+                    ),
+                }
+            )
+        return result
+
+    def get_ibu(self, attributes: List[Dict[str, str]]) -> Union[int, None]:
+        try:
+            value = [i["value"] for i in attributes if i["type"] == "IBU"][0]
+            return int(value)
+        except IndexError:
+            return None
+        except (TypeError, ValueError) as exc:
+            LOG.warning("Unable to parse IBU value in %s: %s", attributes, exc)
+            return None
+
+    def get_abv(self, attributes: List[Dict[str, str]]) -> Union[Decimal, None]:
+        try:
+            value = [i["value"] for i in attributes if i["type"] == "ABV"][0].replace(
+                "%", ""
+            )
+            return Decimal(value)
+        except IndexError:
+            return None
+        except (TypeError, ValueError) as exc:
+            LOG.warning("Unable to parse ABV value in %s: %s", attributes, exc)
+            return None
---
+++ tap_list_providers/test/test_arryved_menu.py
@@ -0,0 +1,87 @@
+"""Test the parsing of Arryved embedded menu data"""
+import os
+import json
+from decimal import Decimal
+
+from django.core.management import call_command
+from django.test import TestCase
+from django.utils.timezone import now
+import responses
+
+from hsv_dot_beer.config.local import BASE_DIR
+from beers.models import Beer, Manufacturer, ManufacturerAlternateName
+from beers.test.factories import ManufacturerFactory
+from venues.test.factories import VenueFactory
+from venues.models import Venue, VenueAPIConfiguration
+from taps.models import Tap
+from tap_list_providers.parsers.arryved_menu import ArryvedMenuParser
+
+
+class CommandsTestCase(TestCase):
+
+    fixtures = ["serving_sizes"]
+
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.manufacturer = ManufacturerFactory()
+        cls.venue = VenueFactory(tap_list_provider=ArryvedMenuParser.provider_name)
+        cls.venue_cfg = VenueAPIConfiguration.objects.create(
+            venue=cls.venue,
+            arryved_location_id="abc123",
+            arryved_menu_id="def456",
+            arryved_manufacturer_name=cls.manufacturer.name,
+            arryved_serving_sizes=[
+                "TAS",
+                "32O",
+                "64O",
+                "win",
+                "13O",
+                "PIN",
+                "POU",
+                "WIN",
+            ],
+        )
+        with open(
+            os.path.join(
+                os.path.dirname(BASE_DIR),
+                "tap_list_providers",
+                "example_data",
+                "arryved_menu.json",
+            ),
+            "rb",
+        ) as json_file:
+            cls.json_data = json.loads(json_file.read())
+
+    @responses.activate
+    def test_parsing(self):
+        responses.add(
+            responses.POST,
+            ArryvedMenuParser.URL,
+            json=self.json_data,
+            status=200,
+        )
+        for dummy in range(2):
+            # running twice to make sure we're not double-creating
+            args = []
+            opts = {}
+            call_command("parsearryvedmenu", *args, **opts)
+            self.assertEqual(Manufacturer.objects.count(), 1)
+            self.assertEqual(Tap.objects.count(), 22)
+            self.assertEqual(Beer.objects.count(), 22)
+            for tap in Tap.objects.all():
+                self.assertEqual(tap.time_updated.year, 2020)
+            tap = Tap.objects.select_related("beer").get(tap_number=2)
+            self.assertEqual(
+                tap.beer.name, "Charmer Tropical IPA"
+            )
+            expected_prices = {
+                Decimal(6): Decimal(3),
+                Decimal(16): Decimal(6),
+            }
+            prices = tap.beer.prices.select_related('serving_size')
+            actual_prices = {
+                price.serving_size.volume_oz: price.price
+                for price in prices
+            }
+            self.assertEqual(expected_prices, actual_prices)
