cec2c5642256af7c4f032ce13398d934f95d2bac
Drew Winstel <drewbrew@users.noreply.github.com>
2021-02-19 21:02:05 -0600

    drop tailwind from installed apps

--- hsv_dot_beer/config/common.py
+++ hsv_dot_beer/config/common.py
@@ -25,7 +25,6 @@ class Common(Configuration):
         "django_filters",  # for filtering rest endpoints
         "django_countries",  # for ease of using countries
         "django_celery_beat",  # use django admin to set up scheduled tasks
-        "tailwind",  # make things less ugly
         "crispy_forms",
         "crispy_tailwind",
         # Your apps
