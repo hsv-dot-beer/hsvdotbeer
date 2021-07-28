2bb4cfa84c688d55d025ced96219589ace9676b1
Drew Winstel <drew@hsv.beer>
2021-07-28 11:20:18 -0500

    Delete taps that aren't in use if we don't have sequential tap numbers
    Fixes #402

---
+++ tap_list_providers/test/test_chattahoochee.py
@@ -0,0 +1,64 @@
+"""Test the parsing of untappd data from Chattahoochee Brewing"""
+import os
+from unittest import mock
+
+from django.core.management import call_command
+from django.test import TestCase
+import responses
+
+from beers.models import Beer, Manufacturer
+from venues.test.factories import VenueFactory
+from venues.models import Venue, VenueAPIConfiguration
+from taps.models import Tap
+from tap_list_providers.parsers.untappd import UntappdParser
+from hsv_dot_beer.config.local import BASE_DIR
+
+
+class CommandsTestCase(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        super().setUpTestData()
+        cls.venue = VenueFactory(tap_list_provider=UntappdParser.provider_name)
+        cls.venue_cfg = VenueAPIConfiguration.objects.create(
+            venue=cls.venue,
+            url="https://localhost:8000",
+            untappd_location=1827,
+            untappd_theme=3788,
+            untappd_categories=["Menu"],
+        )
+        with open(
+            os.path.join(
+                os.path.dirname(BASE_DIR),
+                "tap_list_providers",
+                "example_data",
+                "chattahoochee.html",
+            ),
+            "rb",
+        ) as js_file:
+            cls.js_data = js_file.read()
+
+    @responses.activate
+    @mock.patch("tap_list_providers.base.look_up_beer")
+    def test_clearing_unpopulated_taps(self, _):
+        """Test that we wipe out unpopulated taps"""
+        responses.add(
+            responses.GET,
+            UntappdParser.URL.format(
+                self.venue_cfg.untappd_location,
+                self.venue_cfg.untappd_theme,
+            ),
+            body=self.js_data,
+            status=200,
+        )
+        self.assertFalse(Tap.objects.exists())
+        self.assertEqual(Venue.objects.count(), 1)
+        self.assertFalse(Beer.objects.exists())
+        self.assertFalse(Manufacturer.objects.exists())
+        # create a really large tap number
+        Tap.objects.create(venue=self.venue, tap_number=97)
+        args = []
+        opts = {}
+        call_command("parseuntappd", *args, **opts)
+        self.assertTrue(Beer.objects.exists())
+        # and assert that it's BALEETED
+        self.assertFalse(Tap.objects.filter(venue=self.venue, tap_number=97).exists())
