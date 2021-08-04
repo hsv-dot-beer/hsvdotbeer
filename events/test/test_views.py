38b671b70055d51dff80b0b4c0c2f3802db285ae
Drew Winstel <drewbrew@users.noreply.github.com>
2021-08-03 21:09:34 -0500

    Remove the nose shorthand that isn't really shorthand

--- events/test/test_views.py
+++ events/test/test_views.py
@@ -1,7 +1,6 @@
 import json
 from django.urls import reverse
 from django.forms.models import model_to_dict
-from nose.tools import eq_
 from rest_framework.test import APITestCase
 from rest_framework import status
 from faker import Faker
@@ -40,17 +39,19 @@ class TestEventListTestCase(APITestCase):
             json.dumps({}),
             content_type="application/json",
         )
-        eq_(response.status_code, status.HTTP_400_BAD_REQUEST, response.data)
+        self.assertEqual(
+            response.status_code, status.HTTP_400_BAD_REQUEST, response.data
+        )
 
     def test_post_request_with_valid_data_succeeds(self):
         response = self.client.post(
             self.url, json.dumps(self.event_data), content_type="application/json"
         )
-        eq_(response.status_code, status.HTTP_201_CREATED, response.data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response.data)
 
         event = Event.objects.get(pk=response.data.get("id"))
-        eq_(event.title, self.event_data.get("title"))
-        eq_(event.description, self.event_data.get("description"))
+        self.assertEqual(event.title, self.event_data.get("title"))
+        self.assertEqual(event.description, self.event_data.get("description"))
 
 
 class TestEventDetailTestCase(APITestCase):
@@ -66,13 +67,13 @@ class TestEventDetailTestCase(APITestCase):
 
     def test_get_request_returns_a_given_venue(self):
         response = self.client.get(self.url)
-        eq_(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_patch_request_updates_a_venue(self):
         new_name = fake.first_name()
         payload = {"title": new_name}
         response = self.client.patch(self.url, payload)
-        eq_(response.status_code, status.HTTP_200_OK, response.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK, response.data)
 
         event = Event.objects.get(pk=self.event.id)
-        eq_(event.title, new_name)
+        self.assertEqual(event.title, new_name)
