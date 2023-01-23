f39b5b108b9e9846bd5ea36aa0e33b6596ab7f79
Drew Winstel <drewbrew@users.noreply.github.com>
2023-01-23 17:12:39 -0600

    Update docker container to run the entry point script (#448)
    Fixes #447

--- bin/entry-point.sh
+++ bin/entry-point.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 npm run build
-gunicorn hsv_dot_beer.wsgi --log-file - &
-celery -A hsv_dot_beer worker -l info -c 2 --beat --scheduler django_celery_beat.schedulers:DatabaseScheduler -O fair
+pipenv run python manage.py migrate
+pipenv run python manage.py collectstatic
+pipenv run gunicorn hsv_dot_beer.wsgi --log-file - &
+pipenv run celery -A hsv_dot_beer worker -l info -c 2 --beat --scheduler django_celery_beat.schedulers:DatabaseScheduler -O fair
