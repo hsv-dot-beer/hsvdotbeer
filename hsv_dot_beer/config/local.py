adb4dc051aba1c7757c801b617fe8612614f757f
Drew Winstel <drewbrew@users.noreply.github.com>
2023-02-28 13:41:02 -0600

    bring back local memory cache for test only

--- hsv_dot_beer/config/local.py
+++ hsv_dot_beer/config/local.py
@@ -1,4 +1,6 @@
 import os
+import sys
+
 from .common import Common
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -13,3 +15,11 @@ class Local(Common):
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     # CORS!
     CORS_ORIGIN_WHITELIST = ("https://localhost:8000",)
+
+    # only use locmemcache for manage.py test
+    if sys.argv[1:2] == ["test"]:
+        CACHES = {
+            "default": {
+                "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
+                },
+            }
