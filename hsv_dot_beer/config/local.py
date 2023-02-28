7ebbecf2300e3bc676454f656c80c30807998ec8
Drew Winstel <drewbrew@users.noreply.github.com>
2023-02-28 13:43:58 -0600

    run black

--- hsv_dot_beer/config/local.py
+++ hsv_dot_beer/config/local.py
@@ -21,5 +21,5 @@ class Local(Common):
         CACHES = {
             "default": {
                 "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
-                },
-            }
+            },
+        }
